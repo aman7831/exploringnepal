dc952d23b4932cf6d9b381a51fa7d99d6b96f7c2
aman7831 <amanbhandari188@gmail.com>
2021-05-19 14:20:39 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function opensidemenu(){
+    document.getElementById('side-menu').style.width='250px';
+    document.getElementById('main').style.marginLeft='250px';   
+}
+function closesidemenu(){
+    document.getElementById('side-menu').style.width='0px';
+    document.getElementById('main').style.marginLeft='0px';
+}
